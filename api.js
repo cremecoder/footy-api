7bca0a605f2481d6c5d8f2dad2938e20872abef5
Sean <sean.cremecoder@gmail.com>
2021-09-29 11:20:54 +0100

    removed morgan

--- api.js
+++ api.js
@@ -1,15 +1,11 @@
 const express = require("express")
 const api = express()
-const morgan = require("morgan")
 require("dotenv").config()
 const port = process.env.PORT || 9000
 
 /* Allow Express to recognise requests and JSON */
 api.use(express.urlencoded({ extended: false }))
 api.use(express.json())
-
-/* Morgan listening for http requests */
-api.use(morgan("tiny"))
 
 /* Use apiRouter */
 api.use("/", require("./apiRouter"))
